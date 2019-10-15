fe442d8ffc2c0473085e7b299be6d0dba8991beb
Arup <arupgorai.js@gmail.com>
2019-10-15 23:36:07 +0530

    api added

--- routes/api/items.js
+++ routes/api/items.js
@@ -4,6 +4,7 @@ const router = express.Router();
 // Item Model
 const Item = require('../../models/Items');
 
+// Get req for item
 router.get('/', (req, res) => {
   Item.find()
     .sort({
@@ -13,6 +14,7 @@ router.get('/', (req, res) => {
     .catch(err => console.log(err));
 });
 
+// POST req for item
 router.post('/', (req, res) => {
   const newItem = new Item({
     name: req.body.name
@@ -21,6 +23,13 @@ router.post('/', (req, res) => {
     .save()
     .then(items => res.json(items))
     .catch(err => console.log('Err saving items', err));
+});
+
+// DELETE req for item
+router.delete('/:id', (req, res) => {
+  Item.findById(req.params.id)
+    .then(item => item.remove().then(() => res.json({ success: true })))
+    .catch(err => res.status(404).json({ success: false }));
 });
 
 module.exports = router;
