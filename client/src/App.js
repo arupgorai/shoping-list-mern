0591d44f216fa28ae0fa57e5feb88e1bb07dddcf
Arup <arupgorai.js@gmail.com>
2019-10-21 00:15:17 +0530

    bootstrap modal module added

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { Container } from 'reactstrap';
 import AppNavbar from './components/AppNavbar';
 import ShoppingList from './components/ShoppingList';
+import ItemModal from './components/itemModal';
 import store from './store';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
@@ -11,7 +13,10 @@ function App() {
     <Provider store={store}>
       <div className="App">
         <AppNavbar />
-        <ShoppingList />
+        <Container>
+          <ItemModal />
+          <ShoppingList />
+        </Container>
       </div>
     </Provider>
   );
