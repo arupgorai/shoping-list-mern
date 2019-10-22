f2d3fba87ad8109187fa4044ccc73d36ed6a648c
Arup <arupgorai.js@gmail.com>
2019-10-22 23:26:20 +0530

    preparing to deploy

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const items = require('./routes/api/items');
+const path = require('path');
 
 const app = express();
 
@@ -26,6 +27,16 @@ mongoose
 
 // use routes
 app.use('/api/items', items);
+
+// serve static assets if in production
+if (process.env.NODE_ENV == 'production') {
+  // set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
